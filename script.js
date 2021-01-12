7ea7926a02da45f74e0722381b1e5851042b3310
NelsonBaileyIII <nelsonbailey@Nelsons-MBP.hsd1.ga.comcast.net>
2021-01-12 02:30:34 -0500

    final stages for front page

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
+// window.onload = function(){
+//   var element = document.querySelector('#top');
+//   window.scrollTo(element)
+//   console.log()
+// }
     
 url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 
 $.getJSON(url, function(data){
   console.log(data);
-  $("#quoteDisplay").append("<h7 style='text-align:left'>" + data.quoteText + " - <i>" + data.quoteAuthor + "</i></h7>");
+  $("#quoteDisplay").append("<h7>" + data.quoteText + " - <i>" + data.quoteAuthor + "</i></h7>");
   // shows the code string data. 
   // $("body").append("<code>" + JSON.stringify(data, null, '<br/>') + "</code>");
 });
