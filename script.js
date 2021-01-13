e8f4f2316a6fb33a649a235c4f1f06959182404a
Jennifer Combs <72948870+jco-solutions@users.noreply.github.com>
2021-01-13 12:33:07 -0500

    everything except print settings

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.getAnimations
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
+url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+
+$.getJSON(url, function (data) {
+  console.log(data);
+  $("#quoteDisplay").append("<h7>" + data.quoteText + " - <i>" + data.quoteAuthor + "</i></h7>");
+  // shows the code string data. 
+  // $("body").append("<code>" + JSON.stringify(data, null, '<br/>') + "</code>");
+
+  if (typeof (Storage) !== "undefined") {
+    document.getElementById("result").innerHTML = localStorage.getItem("mentalState");
+  } else {
+    document.getElementById("result").innerHTML = "Click the button to determine your current mental state.";
+  }
+
+  if (typeof (Storage) !== "undefined") {
+    document.getElementById("journalEntryResult").innerHTML = localStorage.getItem("journalEntry");
+  } else {
+    document.getElementById("journalEntryResult").innerHTML = "Click the button to determine your current mental state.";
+  }
+
+});
+
